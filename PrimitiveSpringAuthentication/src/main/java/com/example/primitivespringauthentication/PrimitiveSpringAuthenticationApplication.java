f94510054bd82586a65ceba9be2eed4753751926
Honsage <musatov.i.a@edu.mirea.ru>
2024-12-19 02:16:05 +0300

    feat(PrimitiveSpringAuthentication): add raw web spring template

---
+++ PrimitiveSpringAuthentication/src/main/java/com/example/primitivespringauthentication/PrimitiveSpringAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.example.primitivespringauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimitiveSpringAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimitiveSpringAuthenticationApplication.class, args);
+	}
+
+}
---
+++ PrimitiveSpringAuthentication/src/test/java/com/example/primitivespringauthentication/PrimitiveSpringAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primitivespringauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimitiveSpringAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
