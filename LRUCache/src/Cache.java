675bc014f29ddee60f66bfa740f14add7e2f8a56
Honsage <musatov.i.a@edu.mirea.ru>
2026-06-16 20:38:55 +0300

    perf(lru-cache): improve lru-cache perfomance by wrapping linked hashmap

--- LRUCache/src/Cache.java
+++ LRUCache/src/Cache.java
@@ -1,4 +1,6 @@
 public interface Cache<K, V> {
     void put(K key, V value);
     V get(K key);
+    boolean containsKey(K key);
+    int size();
 }
--- LRUCache/src/LRUCache.java
+++ LRUCache/src/LRUCache.java
@@ -1,61 +1,69 @@
-import java.time.Instant;
-import java.util.HashMap;
-import java.util.Iterator;
+import java.util.Map;
+import java.util.LinkedHashMap;
 import java.util.NoSuchElementException;
 
 public class LRUCache<K, V> implements Cache<K, V> {
 
-    private static final int DEFAULT_CAPACITY = 50;
+    private static final int DEFAULT_CAPACITY = 64;
 
-    private int capacity;
+    private static final float LOAD_FACTOR = 1f;
+
+    private final int capacity;
 
     // Actual in-memory cache storage with O(1) complexity
-    private HashMap<K, V> cache;
-
-    // Last usage time; greater is better
-    private HashMap<K, Instant> lastUsage;
+    private final LinkedHashMapCache<K, V> cache;
 
     public LRUCache(int capacity) {
-        if (capacity < 1) throw new IllegalArgumentException("Idiot!!!");
+        if (capacity < 1)
+            throw new IllegalArgumentException("Capacity must be greater than zero");
         this.capacity = capacity;
-        this.cache = new HashMap<>();
-        this.lastUsage = new HashMap<>();
+        this.cache = new LinkedHashMapCache<>(capacity, LOAD_FACTOR, true);
     }
 
     public LRUCache() {
         this(DEFAULT_CAPACITY);
     }
 
+    @Override
     public void put(K key, V value) {
-        if (cache.size() >= this.capacity
-                && !cache.containsKey(key)) {
-            K keyOfLre = this.keyOfLre();
-            cache.remove(keyOfLre);
-            lastUsage.remove(keyOfLre);
-        }
-        lastUsage.put(key, Instant.now());
-        cache.put(key, value);
+        this.cache.put(key, value);
     }
 
+    @Override
     public V get(K key) {
-        if (!cache.containsKey(key)) {
-            throw new NoSuchElementException(String.format("Cache has no element with key: %s", key.toString()));
-        }
-        lastUsage.put(key, Instant.now());
-        return cache.get(key);
+        if (!this.cache.containsKey(key))
+            throw new NoSuchElementException(String.format("There is no key: %s in cache", key));
+        return this.cache.get(key);
     }
 
-    // O(capacity) complexity
-    private K keyOfLre() {
-        Iterator<K> iter = lastUsage.keySet().iterator();
-        K keyOfLre = iter.next();
-        K cur;
-        while (iter.hasNext()) {
-            cur = iter.next();
-            if (lastUsage.get(keyOfLre).isAfter(lastUsage.get(cur))) {
-                keyOfLre = cur;
-            }
+    @Override
+    public boolean containsKey(K key) {
+        return this.cache.containsKey(key);
+    }
+
+    @Override
+    public int size() {
+        return this.cache.size();
+    }
+
+    public int capacity() {
+        return this.capacity;
+    }
+
+    private static class LinkedHashMapCache<K, V> extends LinkedHashMap<K, V> {
+
+        private final int capacity;
+
+        public LinkedHashMapCache(int capacity,
+                             float loadFactor,
+                             boolean accessOrder) {
+            super(capacity, loadFactor, accessOrder);
+            this.capacity = capacity;
         }
-        return keyOfLre;
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
+            return this.size() > this.capacity;
+        }
     }
 }
